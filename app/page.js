fc891aeb9bd13c11a0766f988bd9af178993cb7d
Rohan <rsharma7_be22@thapar.edu>
2026-04-24 20:37:56 +0530

    fix: hide scroll journey sidebar on mobile

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <ScrollJourney />
+      <div className="scroll-journey-wrap"><ScrollJourney /></div>
       <main>
         <div id="top">
           <Hero />
